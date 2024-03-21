45e2c97b49a1502aee2f211a39dfb3a0f64c3cd7
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2024-03-21 19:14:40 +0100

    💄 style: Add colors based on value to circular progress bar

--- Projet_IOS_Ig4/Festival/Views/CircularProgressView.swift
+++ Projet_IOS_Ig4/Festival/Views/CircularProgressView.swift
@@ -13,7 +13,18 @@ struct CircularProgressView: View {
     let max: Int
     
     var body: some View {
-        VStack {
+        let color: Color
+        if current >= max {
+            color = .red
+        } else if current > max / 2 {
+            color = .orange
+        } else if current >= max / 3 {
+            color = Color(hex: "#ffd500")
+        } else {
+            color = .green
+        }
+        
+        return VStack {
             ZStack {
                 Circle()
                     .stroke(lineWidth: 10.0)
@@ -23,7 +34,7 @@ struct CircularProgressView: View {
                 Circle()
                     .trim(from: 0.0, to: CGFloat(current) / CGFloat(max))
                     .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
-                    .foregroundColor(.blue)
+                    .foregroundColor(color) // Set color based on logic
                     .rotationEffect(Angle(degrees: -90))
                 
                 Text("\(current)/\(max)")
@@ -33,5 +44,26 @@ struct CircularProgressView: View {
             .frame(width: 50, height: 50)
             .padding(.bottom, 5)
         }
+    }
+}
+
+extension Color {
+    // Extension to create Color from hex string
+    init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        if hexSanitized.hasPrefix("#") {
+            hexSanitized.remove(at: hexSanitized.startIndex)
+        }
+        
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        
+        self.init(
+            .sRGB,
+            red: Double((rgb & 0xFF0000) >> 16) / 255.0,
+            green: Double((rgb & 0x00FF00) >> 8) / 255.0,
+            blue: Double(rgb & 0x0000FF) / 255.0,
+            opacity: 1.0
+        )
     }
 }
