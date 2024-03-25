b8a64b2d8bb58d989f5c309085dd35b0d8827671
Lucas Leroy <lucasleroy@MacBook-Pro-de-Lucas.local>
2024-03-25 21:53:51 +0100

    supression pods

--- Pods/Target Support Files/Pods-Projet_IOS_Ig4-Projet_IOS_Ig4UITests/Pods-Projet_IOS_Ig4-Projet_IOS_Ig4UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Projet_IOS_Ig4_Projet_IOS_Ig4UITests : NSObject
-@end
-@implementation PodsDummy_Pods_Projet_IOS_Ig4_Projet_IOS_Ig4UITests
-@end
--- Pods/Target Support Files/Pods-Projet_IOS_Ig4Tests/Pods-Projet_IOS_Ig4Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Projet_IOS_Ig4Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Projet_IOS_Ig4Tests
-@end
